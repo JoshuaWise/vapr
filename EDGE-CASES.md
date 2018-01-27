c9fd090424e68e6ab37b587047c29fe15ae1fcbb
Joshua Wise <joshuathomaswise@gmail.com>
2018-01-27 17:12:57 -0500

    remove connect method; on invalid request, abort the connection

--- EDGE-CASES.md
+++ EDGE-CASES.md
@@ -1,4 +1,5 @@
 # General
+* Request URIs are considered invalid if they are longer than 2048 bytes.
 * Pathnames are considered invalid if they contain any empty path segments, aside from the last.
 * Hostnames are considered invalid if they contain any empty domain labels, aside from the root domain.
 * Port numbers and IP addresses with leading zeros are considered invalid.
--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const PathRouter = require('./lib/path-router');
 const HostRouter = require('./lib/host-router');
 
-// TODO: maybe things like HEAD, OPTIONS, TRACE, or CONNECT should be handled differently
-// for (const m of ['GET', 'CONNECT', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH', 'POST', 'PUT', 'TRACE']) {
+// TODO: maybe things like HEAD, OPTIONS, or TRACE should be handled differently
+// for (const m of ['GET', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH', 'POST', 'PUT', 'TRACE']) {
 // 	const fn = function (pathname, handler) { return this.define(m, pathname, handler) };
 // 	const name = m.toLowerCase();
 // 	Object.defineProperty(fn, 'name', Object.assign(Object.getOwnPropertyDescriptor(fn, 'name'), { value: name }));
