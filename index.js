bd99382527f99ac138d9c321dd2547fd4584e10a
Joshua Wise <joshuathomaswise@gmail.com>
2018-01-28 18:32:19 -0500

    removed comments

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 'use strict';
 const PathRouter = require('./lib/path-router');
 const HostRouter = require('./lib/host-router');
-
-// TODO: maybe things like HEAD, OPTIONS, or TRACE should be handled differently
-// for (const m of ['GET', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH', 'POST', 'PUT', 'TRACE']) {
-// 	const fn = function (pathname, handler) { return this.define(m, pathname, handler) };
-// 	const name = m.toLowerCase();
-// 	Object.defineProperty(fn, 'name', Object.assign(Object.getOwnPropertyDescriptor(fn, 'name'), { value: name }));
-// 	Object.defineProperty(PathRouter.prototype, name, { value: fn, writable: true, enumerable: false, configurable: true });
-// }
+const MethodRouter = require('./lib/method-router');
 
 module.exports = PathRouter;
 module.exports.HostRouter = HostRouter;
+module.exports.MethodRouter = MethodRouter;
