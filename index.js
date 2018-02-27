6878487aadd0c20bf280a1e90ffdf140e1ae67b7
Joshua Wise <joshuathomaswise@gmail.com>
2018-02-27 00:53:25 -0500

    fixed index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
-const PathRouter = require('./lib/path-router');
-const HostRouter = require('./lib/host-router');
-const MethodRouter = require('./lib/method-router');
+const PathRouter = require('./lib/routers/path-router');
+const HostRouter = require('./lib/routers/host-router');
+const MethodRouter = require('./lib/routers/method-router');
+const Route = require('./lib/route');
 
 module.exports = PathRouter;
 module.exports.HostRouter = HostRouter;
 module.exports.MethodRouter = MethodRouter;
+module.exports.Route = Route;
 try { require('wise-inspection')(require('wise-promise')); } catch (_) {}
