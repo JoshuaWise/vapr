fbdd9097d0608184f2b6004e922e7763ee5f5101
Joshua Wise <joshuathomaswise@gmail.com>
2018-06-18 21:06:35 -0400

    reduced url length limit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
-const PathRouter = require('./lib/routers/path-router');
-const HostRouter = require('./lib/routers/host-router');
-const MethodRouter = require('./lib/routers/method-router');
-const Route = require('./lib/route');
+const Application = require('./lib/application/application');
 
-module.exports = PathRouter;
-module.exports.HostRouter = HostRouter;
-module.exports.MethodRouter = MethodRouter;
-module.exports.Route = Route;
+module.exports = Application;
+module.exports.HostRouter = require('./lib/routers/host-router');
+module.exports.PathRouter = require('./lib/routers/path-router');
+module.exports.MethodRouter = require('./lib/routers/method-router');
+module.exports.Request = require('./lib/state/request');
+module.exports.Response = require('./lib/state/response');
+module.exports.Route = require('./lib/route');
 try { require('wise-inspection')(require('wise-promise')); } catch (_) {}
