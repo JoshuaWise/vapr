1bb31e9dc7b2884f9c8baf3b48b19873fbdfe7fc
Joshua Wise <joshuathomaswise@gmail.com>
2018-06-19 00:03:32 -0400

    changed main export to vapr function

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 const Application = require('./lib/application/application');
 
-module.exports = Application;
+module.exports = function vapr(options) { return new Application(options); };
+module.exports.Application = Application;
 module.exports.HostRouter = require('./lib/routers/host-router');
 module.exports.PathRouter = require('./lib/routers/path-router');
 module.exports.MethodRouter = require('./lib/routers/method-router');
+module.exports.Route = require('./lib/route');
 module.exports.Request = require('./lib/state/request');
 module.exports.Response = require('./lib/state/response');
-module.exports.Route = require('./lib/route');
 try { require('wise-inspection')(require('wise-promise')); } catch (_) {}
